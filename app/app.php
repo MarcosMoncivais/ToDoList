a8cf0c6ccdab73d8b4f9e8a762d15a072f95785a
Jose Torres & Jordan Navratil <student@epicodus.com>
2015-08-05 15:42:51 -0700

    fix app.php, everything runs well now. Project completed.

--- app/app.php
+++ app/app.php
@@ -9,8 +9,8 @@
 
     $app = new Silex\Application();
 
-    $app->register(new Silex\Provider\twigServiveProvider(), array(
-        'twig.path' =>__DIR__'./../views'
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
@@ -18,24 +18,15 @@
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->post("/tasks", function() {
-        $task = new Task($_POST['description']);
+    $app->post("/tasks", function() use ($app) {
+        $task = new task($_POST['description']);
         $task->save();
-        return "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-        ";
+        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
     });
 
-    $app->post("/delete_tasks", function() {
-
+    $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-
-        return "
-        <h1>List Cleared!</h1>
-        <p><a href='/''>Home</a></p>
-        ";
+        return $app['twig']->render('delete_tasks.html.twig');
     });
 
     return $app;
